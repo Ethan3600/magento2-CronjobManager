26e6fb607eedc0af06e5a4cf1d76c57892356813
Ethan3600 <Ethan3600@gmail.com>
2017-08-13 13:27:24 -0400

    Add Controller for dashboard

---
+++ EthanYehuda/CronjobManager/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace EthanYehuda\CronjobManager\Controller\Adminhtml\Index;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \Magento\Backend\App\Action\Context $context
+     */
+    public function __construct(
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+    	\Magento\Backend\App\Action\Context $context
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Magento\Backend\App\AbstractAction::_isAllowed()
+     */
+    protected function _isAllowed()
+    {
+    	return $this->_authorization->isAllowed('EthanYehuda_CronjobManager::cronjobmanager');
+    }
+
+    /**
+     * Product list page
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+//         $resultPage->setActiveMenu('EthanYehuda_CronjobManager::index_index');
+        $resultPage->getConfig()->getTitle()->prepend(__('Cron Job Dashboard'));
+        return $resultPage;
+    }
+}
