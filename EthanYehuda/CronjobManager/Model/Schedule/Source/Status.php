7e024b4141e67afa8c451b2a5a210ab7f97363ca
Ethan3600 <Ethan3600@gmail.com>
2017-08-19 19:05:58 -0400

    Add status select menu

---
+++ EthanYehuda/CronjobManager/Model/Schedule/Source/Status.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EthanYehuda\CronjobManager\Model\Schedule\Source;
+
+use \Magento\Cron\Model\Schedule;
+
+class Status implements \Magento\Framework\Data\OptionSourceInterface
+{
+	/**
+	 * Return array of options as value-label pairs
+	 *
+	 * @return array Format: array(array('value' => '<value>', 'label' => '<label>'), ...)
+	 */
+	public function toOptionArray(){
+		return [
+			[
+					'value' => Schedule::STATUS_ERROR,
+					'label' => __('Error')
+			],
+			[
+					'value' => Schedule::STATUS_MISSED,
+					'label' => __('Missed')
+			],
+			[
+					'value' => Schedule::STATUS_PENDING,
+					'label' => __('Pending')
+			],
+			[
+					'value' => Schedule::STATUS_RUNNING,
+					'label' => __('Running')
+			],
+			[
+					'value' => Schedule::STATUS_SUCCESS,
+					'label' => __('Success')
+			],
+		];
+	}
+}
