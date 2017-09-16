3fa44da52a55357cf07349e261eb73e07903d429
Ethan3600 <Ethan3600@gmail.com>
2017-09-16 17:18:12 -0400

    Add Cron actions datasource
    The <actionsColumn> node in the ui component xml file depends on this
    class. It provdes the action urls to the "Action" column by iterating
    through each row in the listing UI component and adding the approprate
    data.
    
    It's important to note that when creating UI components, it is done
    recursively via Magento\Framework\View\Element\UiComponent\Context::prepareDataSource()
    NOT TO BE CONFUSED WITH the prepareDataSource() method implemented in
    the Magento\Framework\View\Element\UiComponentInterface.
    
    Lastly, it should be noted that Context::prepareDataSource() is the one
    doing the recursive method calling to all objects that implement the
    UiComponentInterface. This is a very important distinction between the
    two classes.

---
+++ EthanYehuda/CronjobManager/Ui/Component/Listing/Column/CronActions.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace EthanYehuda\CronjobManager\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\Escaper;
+
+class CronActions extends Column
+{
+    /**
+     * Url path
+     */
+    const URL_PATH_EDIT = 'conjobmanager/manage/edit';
+    const URL_PATH_DELETE = 'conjobmanager/manage_job/delete';
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * Escaper.
+     *
+     * @var Escaper
+     */
+    protected $escaper;
+
+    /**
+     * Constructor
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+    	Escaper $escaper,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        $this->escaper = $escaper;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source.
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item['schedule_id'])) {
+                	$jobCode = $this->escaper->escapeHtml($item['job_code']);
+                    $item[$this->getData('name')] = [
+                        'edit' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_EDIT,
+                                [
+                                    'schedule_id' => $item['schedule_id']
+                                ]
+                            ),
+                            'label' => __('Edit')
+                        ],
+                        'delete' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                static::URL_PATH_DELETE,
+                                [
+                                    'schedule_id' => $item['schedule_id']
+                                ]
+                            ),
+                            'label' => __('Delete'),
+                            'confirm' => [
+                                'job_code' => __('Delete %1', $jobCode),
+                                'message' => __('Are you sure you wan\'t to delete a %1 record?', $jobCode),
+                            ],
+                        ],
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
