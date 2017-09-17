6aa2b57fbeb71b08d890c043b152bd94d0a63dfa
Ethan3600 <Ethan3600@gmail.com>
2017-09-17 17:36:08 -0400

    Add edit page

---
+++ EthanYehuda/CronjobManager/Controller/Adminhtml/Manage/Edit.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace EthanYehuda\CronjobManager\Controller\Adminhtml\Manage;
+
+class Edit extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \Magento\Backend\App\Action\Context $context
+     */
+    public function __construct(
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+    	\Magento\Backend\App\Action\Context $context
+    ) {
+    	parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Magento\Backend\App\AbstractAction::_isAllowed()
+     */
+    protected function _isAllowed()
+    {
+    	return $this->_authorization->isAllowed('EthanYehuda_CronjobManager::cronjobmanager');
+    }
+
+    /**
+     * Product list page
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('EthanYehuda_CronjobManager::cronjobmanager');
+        $resultPage->getConfig()->getTitle()->prepend(__('Edit Cron Job'));
+        return $resultPage;
+    }
+}
