7a78d840e6d2d83af669ae6cac3d03be8c2d8cc5
Ethan3600 <Ethan3600@gmail.com>
2017-09-23 12:15:43 -0400

    Create logic for saving new cron jobs

--- EthanYehuda/CronjobManager/Controller/Adminhtml/Manage/Job/Save.php
+++ EthanYehuda/CronjobManager/Controller/Adminhtml/Manage/Job/Save.php
@@ -2,12 +2,16 @@
 
 namespace EthanYehuda\CronjobManager\Controller\Adminhtml\Manage\Job;
 
+use EthanYehuda\CronjobManager\Model\Manager;
+
 class Save extends \Magento\Backend\App\Action
 {
     /**
      * @var \Magento\Framework\View\Result\PageFactory
      */
     protected $resultPageFactory;
+    
+    protected $cronJobManager;
 
     /**
      * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
@@ -15,10 +19,12 @@ class Save extends \Magento\Backend\App\Action
      */
     public function __construct(
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-    	\Magento\Backend\App\Action\Context $context
+    	\Magento\Backend\App\Action\Context $context,
+    	Manager $cronJobManager
     ) {
     	parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
+        $this->cronJobManager= $cronJobManager;
     }
     
     /**
@@ -37,6 +43,16 @@ class Save extends \Magento\Backend\App\Action
      */
     public function execute()
     {
+    	$jobCode = $this->getRequest()->getParam('job_code');
+    	$scheduledAt = $this->getRequest()->getParam('scheduled_at');
+    	try {
+    		$this->cronJobManager->saveCronJob($jobCode, $scheduledAt);
+    	} catch (\Magento\Framework\Exception\CronException $e) {
+    		$this->getMessageManager()->addErrorMessage($e->getMessage());
+    		$this->_redirect('*/manage/create/');
+    		return;
+    	}
+    	$this->getMessageManager()->addSuccessMessage("Successfully saved Cron Job: {$jobCode}");
     	$this->_redirect('*/manage/index/');
     }
 }
--- EthanYehuda/CronjobManager/Model/Manager.php
+++ EthanYehuda/CronjobManager/Model/Manager.php
@@ -3,8 +3,32 @@
 Namespace EthanYehuda\CronjobManager\Model;
 
 use Magento\Cron\Observer\ProcessCronQueueObserver;
+use \Magento\Cron\Model\Schedule;
 
 class Manager extends ProcessCronQueueObserver
 {
+	public function saveCronJob($jobCode, $time)
+	{
+		$filteredTime = $this->filterTimeInput($time);
+		
+		/**
+		 * @var $schedule \Magento\Cron\Model\Schedule
+		 */
+		$schedule = $this->_scheduleFactory->create()
+			->setJobCode($jobCode)
+			->setStatus(Schedule::STATUS_PENDING)
+			->setCreatedAt(strftime('%Y-%m-%d %H:%M:%S', $this->timezone->scopeTimeStamp()))
+			->setScheduledAt($filteredTime);
+
+		$schedule->getResource()->save($schedule);
+	}
 	
+	protected function filterTimeInput($time) 
+	{
+		$matches = [];
+		preg_match('/(\d+-\d+-\d+)T(\d+:\d+)/', $time, $matches);
+		$yearMonthDate = $matches[1];
+		$hourMinuets = " " . $matches[2];
+		return $yearMonthDate . $hourMinuets;
+	}
 }
