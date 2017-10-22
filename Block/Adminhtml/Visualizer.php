fc0151de917adbe30c4bf1ae771cc0afeb2f9f8d
Ethan3600 <Ethan3600@gmail.com>
2017-10-22 15:14:03 -0400

    Create custom UiComponent for cron timeline
    This wasn't an easy call, but I've decided to go with a UiComponent instead of
    a regular block because this seems to be the way forward with Magento 2. Moreover, there are a
    lot of nice features that come with the knockout data-binding that could
    be useful to leverage.

--- Block/Adminhtml/Visualizer.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace EthanYehuda\CronjobManager\Block\Adminhtml;
-
-class Visualizer extends \Magento\Backend\Block\Template
-{
-    function _prepareLayout(){}
-}
---
+++ view/adminhtml/web/js/cronjobmanager_visualizer_component.js
@@ -0,0 +1,9 @@
+define(['uiElement','ko'], function(Element, ko){
+    viewModelConstructor = Element.extend({
+        defaults: {
+            
+        }
+    });
+
+    return viewModelConstructor;
+});
