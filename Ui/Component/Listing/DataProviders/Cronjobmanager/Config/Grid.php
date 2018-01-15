2b02c44e4584be396dcf76bc6ccee534e10a235d
Ethan3600 <Ethan3600@gmail.com>
2018-01-15 15:41:40 -0500

    Implement pseudo collection using config data
    By nature of its interface, dataproviders were made to working with a
    collection. What we did here is remove the need for collections, by
    "reimplementing" our own way of sorting and paginating.
    
    We also implemented a cache to save records because when a user sorts,
    or paginates, it will call the getData() method every time. This will
    make the component feel sluggish because we need to call a double nested
    loop every time we want to sort or paginate.

--- Ui/Component/Listing/DataProviders/Cronjobmanager/Config/Grid.php
+++ Ui/Component/Listing/DataProviders/Cronjobmanager/Config/Grid.php
@@ -1,12 +1,61 @@
 <?php
+
 namespace EthanYehuda\CronjobManager\Ui\Component\Listing\DataProviders\Cronjobmanager\Config;
 
-use EthanYehuda\CronjobManager\Model\Manager;
-use Magento\Cron\Model\ResourceModel\Schedule\CollectionFactory;
+use EthanYehuda\CronjobManager\Model\ManagerFactory;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+use Magento\Framework\App\CacheInterface;
+use Magento\Framework\App\Cache\Type\Collection;
+use Magento\Framework\Serialize\SerializerInterface;
+use Magento\Framework\App\Cache\StateInterface;
 
-class Grid extends \Magento\Ui\DataProvider\AbstractDataProvider
+class Grid extends AbstractDataProvider
 {
-	private $loadedData;
+	const JOB_CONFIG_IDENTIFIER = 'ETHANYEHUDA_CRONJOBMANAGER_DATAPROVIDER_RECORDS';
+	
+	/**
+	 * @var int cache lifetime in seconds
+	 */
+	const CACHE_LIFETIME = 180;
+	
+	/**
+	 * Page size
+	 * 
+	 * @var int
+	 */
+	private $pageSize = 20;
+	
+	/**
+	 * Pagination number
+	 * 
+	 * @var int
+	 */
+	private $pageNum = 1;
+	
+	/**
+	 * @var string
+	 */
+	private $sortedColumn = 'job_code';
+	
+	/**
+	 * @var array
+	 */
+	private $records = [];
+	
+	/**
+	 * @var CacheInterface
+	 */
+	private $cache;
+	
+	/**
+	 * @var SerializerInterface
+	 */
+	private $serializer;
+	
+	/**
+	 * @var StateInterface
+	 */
+	private $cacheState;
 	
 	/**
 	 * @var EthanYehuda\CronjobManager\Model\Manager $manager
@@ -17,46 +66,107 @@ class Grid extends \Magento\Ui\DataProvider\AbstractDataProvider
         $name,
         $primaryFieldName,
         $requestFieldName,
-    	CollectionFactory $collectionFactory,
+    	ManagerFactory $manager,
+    	CacheInterface $cache,
+    	SerializerInterface $serializer,
+    	StateInterface $cacheState,
         array $meta = [],
-        array $data = [],
-    	Manager $manager
+        array $data = []
     ) {
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
-        $this->manager = $manager;
-        $this->collection = $collectionFactory->create();
+        $this->manager = $manager->create();
+        $this->cache = $cache;
+        $this->serializer = $serializer;
+        $this->cacheState = $cacheState;
     }
     
     public function getData()
     {
-    	if($this->loadedData) {
-    		return $this->loadedData;
+    	if($this->cacheState->isEnabled(Collection::TYPE_IDENTIFIER)
+    		&& $loadedData = $this->cache->load(self::JOB_CONFIG_IDENTIFIER)
+    	) {
+    		$this->records = $this->serializer->unserialize($loadedData);
+    	} else {
+	    	$this->prepareJobConfigRecords();
+	    	
+	    	$this->cache->save(
+	    		$this->serializer->serialize($this->records),
+	    		self::JOB_CONFIG_IDENTIFIER, 
+	    		[Collection::CACHE_TAG],
+	    		self::CACHE_LIFETIME
+	    	);
     	}
+    	
+    	// @todo create sorting function
+    	
+    	$this->paginate();
+    	return $this->records;
+    }
+    
+    /**
+     * Sets limits on pagination size
+     * 
+     * @param type $offset
+     * @param type $size
+     */
+    public function setLimit($offset, $size)
+    {
+    	$this->pageSize = $size;
+    	$this->pageNum = $offset;
+    }
+    
+    /**
+     * Set the sort order
+     * 
+     * @todo we need to override this method to avoid errors,
+     * but this also means we need to implement our own sorting
+     * mechanisim
+     * 
+     * @param type $field
+     * @param type $direction
+     */
+    public function addOrder($col, $dir)
+    {
+    	$this->sortedColumn = $col;
+    	$this->sortDirection = strtolower($dir);
+    }
+    
+    private function prepareJobConfigRecords()
+    {
+    	$this->records = [
+    			'totalRecords' => 0,
+    			'items' => []
+    	];
     	
     	$jobs = $this->manager->getCronJobs();
     	
     	foreach ($jobs as $group => $crons) {
     		foreach ($crons as $code => $job) {
+    			$this->records['totalRecords']++;
     			$instance = $job['instance'];
     			$method = $job['method'];
-    			$schedule = (isset($job['schedule']) ? $job['schedule'] : "");
+    			$frequency= (isset($job['schedule']) ? $job['schedule'] : "");
     			$jobData = [
-    				$code,
-    				$group,
-    				$schedule,
-    				"$instance::$method"
+    					'job_code' => $code,
+    					'group' => $group,
+    					'frequency' => $frequency,
+    					'class' => "$instance::$method()"
     			];
     			
-    			$this->loadedData[$code] = $jobData;
+    			array_push($this->records['items'], $jobData);
     		}
     	}
-    	
-    	return $this->loadedData;
     }
     
-    public function getMeta()
+    /**
+     * Limits the amount of items provided to the UiComponent
+     */
+    private function paginate()
     {
-    	$meta = parent::getMeta();
-    	return $meta;
+    	$this->records['items'] = array_slice(
+    		$this->records['items'],
+    		(($this->pageNum - 1) * $this->pageSize),
+    		$this->pageSize
+    	);
     }
 }
