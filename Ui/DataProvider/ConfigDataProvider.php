50d5e4b165252a0e9a8ee0083379dd4e0b707072
Ethan3600 <Ethan3600@gmail.com>
2018-01-28 18:30:22 -0500

    Create data provider for edit page

---
+++ Ui/DataProvider/ConfigDataProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace EthanYehuda\CronjobManager\Ui\DataProvider;
+
+use EthanYehuda\CronjobManager\Model\RegistryConstants;
+use Magento\Cron\Model\ResourceModel\Schedule\CollectionFactory;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+use Magento\Framework\Registry;
+
+class ConfigDataProvider extends AbstractDataProvider
+{
+    private $loadedData;
+    
+    /**
+     * @var Magento\Framework\Registry;
+     */
+    private $coreRegistry;
+
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $collectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        Registry $coreRegistry,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+        $this->coreRegistry = $coreRegistry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData()
+    {
+        if ($this->loadedData) {
+            return $this->loadedData;
+        }
+
+        $this->loadedData = $this->coreRegistry->registry(
+            RegistryConstants::CURRENT_CRON_CONFIG
+        );
+
+        return $this->loadedData;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMeta()
+    {
+        $meta = parent::getMeta();
+        return $meta;
+    }
+}
