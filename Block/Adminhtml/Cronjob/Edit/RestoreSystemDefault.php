441e43c0fca32c8fb1363d111fa004ddb845ed35
Ethan3600 <Ethan3600@gmail.com>
2018-02-10 00:19:41 -0500

    Add system restore controller and button
    The block allows us to pass parameters from the registry to the
    controller. Otherwise we wouldn't be able to pass data

---
+++ Block/Adminhtml/Cronjob/Edit/RestoreSystemDefault.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EthanYehuda\CronjobManager\Block\Adminhtml\Cronjob\Edit;
+
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+class RestoreSystemDefault extends GenericButton implements ButtonProviderInterface
+{
+    /**
+     * @return array
+     */
+    public function getButtonData()
+    {
+        $params = $this->getRequestParams();
+        unset($params['key'], $params['form_key']);
+        $data = [
+            'label' => __('Restore System Default'),
+            'class' => 'secondary',
+            'on_click' => 'deleteConfirm(\''
+            . __('Are you sure you want to restore this to system defaults?')
+            . '\', \''
+            . $this->urlBuilder->getUrl('*/config_job/restoreSystemDefault', $params)
+            . '\')',
+            'sort_order' => 5,
+        ];
+        return $data;
+    }
+}
