76d572e1893c9622da438fe769af683f63f25617
Ethan3600 <Ethan3600@gmail.com>
2018-04-08 18:46:47 -0400

    Use uiComponent for timeline

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,10 +1,5 @@
 var config = {
 	paths: {
         'cronjobManager/template': 'EthanYehuda_CronjobManager/templates'
-    },
-    map: {
-        '*': {
-            eyTimeline:      'EthanYehuda_CronjobManager/js/timeline/timeline',
-        }
     }
 };
