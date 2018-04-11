d994eaae69e1dc07b46b5b8e4985f8131e62311a
Ethan3600 <Ethan3600@gmail.com>
2018-04-10 21:59:57 -0400

    Calculate date range

--- Ui/DataProvider/TimelineDataProvider.php
+++ Ui/DataProvider/TimelineDataProvider.php
@@ -37,10 +37,25 @@ class TimelineDataProvider extends AbstractDataProvider
         if ($this->loadedData) {
             return $this->loadedData;
         }
+
+        $firstHour = null;
+        $lastHour = null;
+
         $this->collection->addOrder('job_code');
         foreach ($this->collection->getItems() as $item) {
             $this->loadedData[$item->getJobCode()][] = $item->getData();
+            
+            $minimumTime = $this->getFirstHour($item);
+            $firstHour = is_null($firstHour) ?
+                $minimumTime: min($firstHour, $this->getFirstHour($item));
+
+            $lastHour  = is_null($lastHour) ?
+                $minimumTime: max($lastHour, $this->getLastHour($item));
         }
+
+        array_unshift($this->loadedData, [
+            'range' => $this->getRange($firstHour, $lastHour)
+        ]);
 
         return $this->loadedData;
     }
@@ -52,5 +67,30 @@ class TimelineDataProvider extends AbstractDataProvider
     {
         $meta = parent::getMeta();
         return $meta;
+    }
+
+    private  function getRange($firstHour, $lastHour)
+    {
+        $firstHour = strtotime($firstHour);
+        $lastHour = strtotime($lastHour);
+        return [
+            'first' => $firstHour,
+            'last' => $lastHour
+        ];
+    }
+
+    private function getFirstHour($item)
+    {
+        $firstHour = $item->getExecutedAt();
+        if (empty($firstHour)) {
+            $firstHour = $item->getScheduledAt();
+        }
+        return $firstHour ;
+    }
+
+    private function getLastHour($item)
+    {
+        $lastHour = $item->getFinishedAt();
+        return $lastHour;
     }
 }
