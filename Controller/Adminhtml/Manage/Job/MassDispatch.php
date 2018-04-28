82ddb8b98085c9b8d2967bc5a0ec215e20183e38
Ethan3600 <Ethan3600@gmail.com>
2018-04-28 14:03:40 -0400

    Create Mass Dispatch

---
+++ Controller/Adminhtml/Manage/Job/MassDispatch.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace EthanYehuda\CronjobManager\Controller\Adminhtml\Manage\Job;
+
+use EthanYehuda\CronjobManager\Model\ManagerFactory;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Backend\App\Action;
+
+class MassDispatch extends Action
+{
+    const ADMIN_RESOURCE = "EthanYehuda_CronjobManager::cronjobmanager";
+    
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    private $resultPageFactory;
+    
+    /**
+     * @var ManagerFactory
+     */
+    private $managerFactory;
+    
+    public function __construct(
+        ManagerFactory $managerFactory,
+        PageFactory $resultPageFactory,
+        Context $context
+    ) {
+        parent::__construct($context);
+        $this->managerFactory = $managerFactory;
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Save cronjob
+     *
+     * @return Void
+     */
+    public function execute()
+    {
+        $manager = $this->managerFactory->create();
+        $params = $this->getRequest()->getParam('selected');
+        if (!isset($params)) {
+            $this->getMessageManager()->addErrorMessage("Something went wrong when recieving the request");
+            $this->_redirect('*/manage/index');
+            return;
+        }
+        try {
+            foreach ($params as $jobId) {
+                $manager->dispatchCron($jobId);
+            }
+        } catch (\Exception $e) {
+            $this->getMessageManager()->addErrorMessage($e->getMessage());
+            $this->_redirect('*/manage/index/');
+            return;
+        }
+        $this->getMessageManager()->addSuccessMessage("Successfully Deleted Schedules");
+        $this->_redirect("*/manage/index/");
+    }
+}
