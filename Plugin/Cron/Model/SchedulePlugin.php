bab6a3fe1e2cd4257b0ee8cc10e700d7d8ab73d3
AndreKlang <andre.klang@evalent.com>
2018-05-07 16:15:34 +0200

    Save pid to schedule A.S.A.P, in case the process is killed

--- Plugin/Cron/Model/SchedulePlugin.php
+++ Plugin/Cron/Model/SchedulePlugin.php
@@ -4,6 +4,15 @@ namespace EthanYehuda\CronjobManager\Plugin\Cron\Model;
 
 class SchedulePlugin
 {
+    /** @var \Magento\Cron\Model\ResourceModel\Schedule */
+    private $resourceModel;
+
+    public function __construct(
+        \Magento\Cron\Model\ResourceModel\Schedule $resourceModel
+    ) {
+        $this->resourceModel = $resourceModel;
+    }
+
     /**
      * If the return from @see \Magento\Cron\Model\Schedule::tryLockJob is
      * true, the job has started in THIS process, if it returns false, it has
@@ -12,11 +21,13 @@ class SchedulePlugin
      * @param \Magento\Cron\Model\Schedule $subject
      * @param $return
      * @return boolean
+     * @throws \Magento\Framework\Exception\AlreadyExistsException
      */
     public function afterTryLockJob(\Magento\Cron\Model\Schedule $subject, $return)
     {
         if ($return) {
             $subject->setData("pid", \getmypid());
+            $this->resourceModel->save($subject); // Save A.S.A.P, in case the process is killed
         }
 
         return $return;
