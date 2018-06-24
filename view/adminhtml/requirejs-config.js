614892ea723fa0b9344410a77b2dc5d90cd12792
Ethan3600 <Ethan3600@gmail.com>
2018-06-24 14:47:55 -0400

    Force requirejs to load virtual foreach first

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,5 +1,10 @@
 var config = {
 	paths: {
         'cronjobManager/template': 'EthanYehuda_CronjobManager/templates'
+    },
+    shim: {
+        'EthanYehuda/js/timeline/timeline': {
+            'deps': ['EthanYehuda/js/lib/knockout/bindings/virtual-foreach']
+        }
     }
 };
