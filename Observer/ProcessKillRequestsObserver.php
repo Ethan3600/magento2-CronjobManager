c2912efeca2c855be6c20ad016029872584e5024
Fabian Schmengler <fs@integer-net.de>
2019-02-10 10:57:51 +0100

    Add integration test for CleanRunningJobsObserver

--- Observer/ProcessKillRequestsObserver.php
+++ Observer/ProcessKillRequestsObserver.php
@@ -10,10 +10,8 @@ class ProcessKillRequestsObserver implements ObserverInterface
 {
     public function execute(Observer $observer)
     {
-        // TODO: write integration test for CleanRunningJobsObserver
         // TODO: extract logic from CleanRunningJobsObserver to service
         // TODO: based on that, write service to process kill requests
         // TODO: call new service from ProcessKillRequestsObserver
     }
-
 }
---
+++ Test/Integration/CleanDeadJobsTest.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace EthanYehuda\CronjobManager\Test\Integration;
+
+use Magento\Cron\Model\Schedule;
+use Magento\Framework\ObjectManager\ObjectManager;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+use Magento\Framework\Event;
+
+/**
+ * @magentoAppArea crontab
+ */
+class CleanDeadJobsTest extends TestCase
+{
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @var Event\ManagerInterface
+     */
+    private $eventManager;
+
+    private const DEAD_PID = 99999999;
+
+    protected function setUp()
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->eventManager = $this->objectManager->get(Event\ManagerInterface::class);
+    }
+
+    public function testDeadRunningJobsAreCleaned()
+    {
+        $this->givenRunningScheduleWithInactiveProcess($schedule);
+        $this->whenEventIsDispatched('process_cron_queue_before');
+        $this->thenScheduleHasStatus($schedule, Schedule::STATUS_ERROR);
+        $this->andScheduleHasMessage($schedule, 'Process went away');
+    }
+
+    public function testActiveRunningJobsAreNotCleaned()
+    {
+        $this->givenRunningScheduleWithActiveProcess($schedule);
+        $this->whenEventIsDispatched('process_cron_queue_before');
+        $this->thenScheduleHasStatus($schedule, Schedule::STATUS_RUNNING);
+    }
+
+    private function givenRunningScheduleWithInactiveProcess(&$schedule)
+    {
+        /** @var Schedule $schedule */
+        $schedule = $this->objectManager->create(Schedule::class);
+        $schedule->setStatus(Schedule::STATUS_RUNNING);
+        $schedule->setData('pid', self::DEAD_PID);
+        $schedule->save();
+    }
+
+    private function givenRunningScheduleWithActiveProcess(&$schedule)
+    {
+        /** @var Schedule $schedule */
+        $schedule = $this->objectManager->create(Schedule::class);
+        $schedule->setStatus(Schedule::STATUS_RUNNING);
+        $schedule->setData('pid', \getmypid());
+        $schedule->save();
+    }
+
+    private function whenEventIsDispatched($eventName)
+    {
+        $this->eventManager->dispatch($eventName);
+    }
+
+    private function thenScheduleHasStatus(Schedule $schedule, $expectedStatus)
+    {
+        /** @var \Magento\Cron\Model\ResourceModel\Schedule $scheduleResource */
+        $scheduleResource = $this->objectManager->get(\Magento\Cron\Model\ResourceModel\Schedule::class);
+        $scheduleResource->load($schedule, $schedule->getId());
+        $this->assertEquals($expectedStatus, $schedule->getStatus(), 'Schedule should have expected status');
+    }
+
+    private function andScheduleHasMessage(Schedule $schedule, $expectedMessage)
+    {
+        /** @var \Magento\Cron\Model\ResourceModel\Schedule $scheduleResource */
+        $scheduleResource = $this->objectManager->get(\Magento\Cron\Model\ResourceModel\Schedule::class);
+        $scheduleResource->load($schedule, $schedule->getId());
+        $this->assertEquals($expectedMessage, $schedule->getMessages(), 'Schedule should have expected message');
+    }
+}
