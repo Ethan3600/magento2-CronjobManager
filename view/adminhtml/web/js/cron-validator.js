b86415a23ba47259413c672df7c7145565f9e62c
Adam Mellen <github@mellen.io>
2019-10-08 09:57:31 +0100

    Consistent formatting - changes look odd in GitHub web view
    Signed-off-by: Adam Mellen <github@mellen.io>

--- view/adminhtml/web/js/cron-validator.js
+++ view/adminhtml/web/js/cron-validator.js
@@ -1,24 +1,24 @@
 require(
-[
-	'Magento_Ui/js/lib/validation/validator',
-	'jquery',
-	'mage/translate'
-],
-function(validator, $) {
+    [
+        'Magento_Ui/js/lib/validation/validator',
+        'jquery',
+        'mage/translate'
+    ],
+    function (validator, $) {
 
-	const CRON_REGEXES = [
-        /((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})/i, //Regular regex terms
-		/@(annually|yearly|monthly|weekly|daily|hourly|reboot)/i, //Predefined cron macros (non-standard)
-		/@every (\d+(ns|us|µs|ms|s|m|h))+/i, //Other cron macros
-	];
+        const CRON_REGEXES = [
+            /((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})/i, //Regular regex terms
+            /@(annually|yearly|monthly|weekly|daily|hourly|reboot)/i, //Predefined cron macros (non-standard)
+            /@every (\d+(ns|us|µs|ms|s|m|h))+/i, //Other cron macros
+        ];
 
-    validator.addRule(
-        'cron-validation',
-        function(value) {
-        	for (let regex in CRON_REGEXES) {
-        		if (regex.test(value)) return true;
-			}
-        	return false;
-        }, $.mage.__('Invalid cron expression. Please try again.')
-    );
-});
+        validator.addRule(
+            'cron-validation',
+            function (value) {
+                for (let regex in CRON_REGEXES) {
+                    if (regex.test(value)) return true;
+                }
+                return false;
+            }, $.mage.__('Invalid cron expression. Please try again.')
+        );
+    });
