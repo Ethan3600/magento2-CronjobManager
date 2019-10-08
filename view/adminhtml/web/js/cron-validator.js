dc8213ed885358163809e767fb26c1a9b77fe164
Adam Mellen <github@mellen.io>
2019-10-08 09:50:14 +0100

    Changing cron-validator to use a more streamlined approach with better targeting regexes
    Original regexes from https://stackoverflow.com/a/57639657/10139735
    
    Signed-off-by: Adam Mellen <github@mellen.io>

--- view/adminhtml/web/js/cron-validator.js
+++ view/adminhtml/web/js/cron-validator.js
@@ -5,11 +5,20 @@ require(
 	'mage/translate'
 ],
 function(validator, $) {
+
+	const CRON_REGEXES = [
+        /((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})/i, //Regular regex terms
+		/@(annually|yearly|monthly|weekly|daily|hourly|reboot)/i, //Predefined cron macros (non-standard)
+		/@every (\d+(ns|us|µs|ms|s|m|h))+/i, //Other cron macros
+	];
+
     validator.addRule(
         'cron-validation',
         function(value) {
-        	var regex = /^$|(\*|[0-5]?[0-9]|\*\/[0-9]+)\s+(\*|1?[0-9]|2[0-3]|\*\/[0-9]+)\s+(\*|[1-2]?[0-9]|3[0-1]|\*\/[0-9]+)\s+(\*|[0-9]|1[0-2]|\*\/[0-9]+)\s+(\*\/[0-9]+|\*|[0-7])\s*(\*\/[0-9]+|\*|[0-9]+)?/i;
-        	return regex.test(value);
+        	for (let regex in CRON_REGEXES) {
+        		if (regex.test(value)) return true;
+			}
+        	return false;
         }, $.mage.__('Invalid cron expression. Please try again.')
     );
 });
