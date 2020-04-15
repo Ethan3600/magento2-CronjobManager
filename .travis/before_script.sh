98435849490d1ef1a3418b138161f336937fcf76
Dan Wallis <mrdanwallis@gmail.com>
2020-04-15 12:17:29 +0100

    Correct Travis CI branch test

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -27,7 +27,7 @@ MY_REPO_SLUG=${TRAVIS_PULL_REQUEST_SLUG:-$TRAVIS_REPO_SLUG}
 MY_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 composer config minimum-stability dev
 composer config repositories.travis_to_test git https://github.com/${MY_REPO_SLUG}.git
-case $TRAVIS_BRANCH in
+case $MY_BRANCH in
     "1.x" | "0.x")
         composer require ${COMPOSER_PACKAGE_NAME}:${MY_BRANCH}-dev\#{$TRAVIS_COMMIT}
         ;;
