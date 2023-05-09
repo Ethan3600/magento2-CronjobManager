e63de93f80bdfbb25b037f364847f5f537ea6af4
Dan Wallis <dan@wallis.nz>
2023-05-09 16:37:38 +0100

    Rewrite expression to pass all provided examples

--- view/adminhtml/web/js/cron-validator.js
+++ view/adminhtml/web/js/cron-validator.js
@@ -7,7 +7,7 @@ require(
     function (validator, $) {
 
         const CRON_REGEXES = [
-            /^((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})$/i, //Regular regex terms
+            /^(\*|[0-5]?\d(-[0-5]?\d)?(,[0-5]?\d(-[0-5]?\d)?)*)(\/\d+)?\s+(\*|([01]?\d|2[0-3])(-([01]?\d|2[0-3]))?(,([01]?\d|2[0-3])(-([01]?\d|2[0-3]))?)*)(\/\d+)?\s+(\*|([0-2]?\d|3[01])(-([0-2]?\d|3[01]))?(,([0-2]?\d|3[01])(-([0-2]?\d|3[01]))?)*)(\/\d+)?\s+(\*|(0?\d|1[0-2])(-(0?\d|1[0-2]))?(,(0?\d|1[0-2])(-(0?\d|1[0-2]))?)*)(\/\d+)?\s+(\*|0?[0-7](-0?[0-7])?(,0?[0-7](-0?[0-7])?)*)(\/\d+)?$/i, //Regular regex terms - m h dom mon dow
             /^@(annually|yearly|monthly|weekly|daily|hourly|reboot)$/i, //Predefined cron macros (non-standard)
             /^@every (\d+(ns|us|µs|ms|s|m|h))+$/i, //Other cron macros
         ];
