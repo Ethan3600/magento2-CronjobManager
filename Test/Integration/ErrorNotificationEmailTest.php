455b34ce7bb4eddb842def213bc6e798966be1f0
Peter Jaap Blaakmeer <peterjaap@blaakmeer.com>
2023-12-05 22:27:47 +0100

    Use Laminas namespaced Mime Decode instead of Zend_Mime_Decode (#204)

--- Test/Integration/ErrorNotificationEmailTest.php
+++ Test/Integration/ErrorNotificationEmailTest.php
@@ -119,7 +119,7 @@ class ErrorNotificationEmailTest extends TestCase
     private function thenEmailShouldBeSent(?Message $sentMessage, string $expectedSender, array $expectedRecipients)
     {
         $this->assertNotNull($sentMessage, 'A mail should have been sent');
-        $messageDetails = \Zend\Mail\Message::fromString($sentMessage->getRawMessage());
+        $messageDetails = \Laminas\Mail\Message::fromString($sentMessage->getRawMessage());
         $this->assertEquals([$expectedSender], \array_keys(\iterator_to_array($messageDetails->getFrom())));
         $this->assertEquals($expectedRecipients, \array_keys(\iterator_to_array($messageDetails->getTo())));
     }
@@ -132,7 +132,7 @@ class ErrorNotificationEmailTest extends TestCase
     private function andEmailShouldHaveContents(Message $sentMessage, array $expectedContents): void
     {
         $content = $sentMessage->getBody()->getParts()[0]->getContent();
-        $content = \Zend_Mime_Decode::decodeQuotedPrintable($content);
+        $content = \Laminas\Mime\Decode::decodeQuotedPrintable($content);
         foreach ($expectedContents as $expectedKey => $expectedContent) {
             if (\method_exists($this, 'assertStringContainsString')) {
                 $this->assertStringContainsString($expectedContent, $content, "Content should contain $expectedKey");
